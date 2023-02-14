1d55e6e82b697acfa1c50c1fbe4d51097cbe9f88
jackdelv <jackdelv39@gmail.com>
2023-02-14 09:53:45 -0500

    Removed old code. Currently in working state though not tested very much.

--- plugins/parquet/parquetembed.hpp
+++ plugins/parquet/parquetembed.hpp
@@ -712,33 +712,6 @@ namespace parquetembed
                 }
                 else
                 {
-                    // arrow::MemoryPool* pool = arrow::default_memory_pool();
-
-                    //   // Configure general Parquet reader settings
-                    //   auto reader_properties = parquet::ReaderProperties(pool);
-                    //   reader_properties.set_buffer_size(4096 * 4);
-                    //   reader_properties.enable_buffered_stream();
-
-                    //   // Configure Arrow-specific Parquet reader settings
-                    //   auto arrow_reader_props = parquet::ArrowReaderProperties();
-                    //   arrow_reader_props.set_batch_size(128 * 1024);  // default 64 * 1024
-
-                    //   parquet::arrow::FileReaderBuilder reader_builder;
-                    //   ARROW_RETURN_NOT_OK(
-                    //       reader_builder.OpenFile(path_to_file, /*memory_map=*/false, reader_properties));
-                    //   reader_builder.memory_pool(pool);
-                    //   reader_builder.properties(arrow_reader_props);
-
-                    //   std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
-                    //   ARROW_ASSIGN_OR_RAISE(arrow_reader, reader_builder.Build());
-
-                    //   std::shared_ptr<::arrow::RecordBatchReader> rb_reader;
-                    //   ARROW_RETURN_NOT_OK(arrow_reader->GetRecordBatchReader(&rb_reader));
-
-                    //   for (arrow::Result<std::shared_ptr<arrow::RecordBatch>> maybe_batch : *rb_reader) {
-                    //     // Operate on each batch...
-                    //   }
-
                     PARQUET_ASSIGN_OR_THROW(infile, arrow::io::ReadableFile::Open(p_location));
 
                     PARQUET_THROW_NOT_OK(parquet::arrow::OpenFile(infile, arrow::default_memory_pool(), &parquet_read));

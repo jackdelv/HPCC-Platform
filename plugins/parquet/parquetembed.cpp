cdcb3a595d1b7f1d587202c096a6d604898631b3
Jack Del Vecchio <jackdelv39@gmail.com>
2023-02-28 17:30:55 +0000

    Added new comment.

--- plugins/parquet/parquetembed.cpp
+++ plugins/parquet/parquetembed.cpp
@@ -569,6 +569,11 @@ namespace parquetembed
         return count;
     }
 
+    /**
+    * @brief Creates an arrow::Schema from the field info of the row.
+    * @param typeInfo An RtlTypeInfo object that we iterate through to get all
+    * the information for the row.
+    */
     arrow::Status ParquetHelper::fieldsToSchema(const RtlTypeInfo *typeInfo)
     {
         const RtlFieldInfo *const *fields = typeInfo->queryFields();

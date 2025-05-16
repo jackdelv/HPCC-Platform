9181006898d8e40e1f527f4c3a2a20b59e16ae3d
Gavin Halliday <gavin.halliday@lexisnexis.com>
2025-05-16 14:59:44 +0100

    HPCC-34222 Further refactoring of the stream reading classes
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- fs/dafsserver/dafsserver.cpp
+++ fs/dafsserver/dafsserver.cpp
@@ -1410,7 +1410,7 @@ public:
 };
 
 
-class CRemoteStreamReadBaseActivity : public CRemoteDiskBaseActivity, implements IFileSerialStreamCallback
+class CRemoteStreamReadBaseActivity : public CRemoteDiskBaseActivity
 {
     typedef CRemoteDiskBaseActivity PARENT;
 
@@ -1423,6 +1423,7 @@ protected:
     bool cursorDirty = false;
     bool fetching = false;
     unsigned __int64 bytesRead = 0;
+    unsigned __int64 lastBytesRead = 0;
     // virtual field values
     unsigned partNum = 0;
     offset_t baseFpos = 0;
@@ -1476,7 +1477,7 @@ protected:
                 compressed = false;
             }
         }
-        inputStream.setown(createFileSerialStream(iFileIO, startPos, (offset_t)-1, (size32_t)-1, this));
+        inputStream.setown(createFileSerialStream(iFileIO, startPos, (offset_t)-1, (size32_t)-1));
 
         opened = true;
         eofSeen = false;
@@ -1484,14 +1485,11 @@ protected:
     }
     void close()
     {
+        if (iFileIO)
+            bytesRead += iFileIO->getStatistic(StSizeDiskRead);
         iFileIO.clear();
         opened = false;
         eofSeen = true;
-    }
-// IFileSerialStreamCallback impl.
-    virtual void process(offset_t ofs, size32_t sz, const void *buf) override
-    {
-        bytesRead += sz;
     }
 public:
     CRemoteStreamReadBaseActivity(IPropertyTree &config, IFileDescriptor *fileDesc) : PARENT(config, fileDesc)
@@ -1505,8 +1503,11 @@ public:
     virtual void flushStatistics(CClientStats &stats) override
     {
         // NB: will be called by same thread that is reading.
-        stats.addRead(bytesRead);
-        bytesRead = 0;
+        offset_t totalBytesRead = bytesRead;
+        if (iFileIO)
+            totalBytesRead += iFileIO->getStatistic(StSizeDiskRead);
+        stats.addRead(totalBytesRead - lastBytesRead);
+        lastBytesRead = totalBytesRead;
     }
 // IRemoteReadActivity impl.
     virtual void seek(offset_t pos) override

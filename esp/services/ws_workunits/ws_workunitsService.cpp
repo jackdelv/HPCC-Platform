14a25a2b234bad7647c5b8b4b4e7856645c737df
Gavin Halliday <gavin.halliday@lexisnexis.com>
2026-01-27 09:10:09 +0000

    HPCC-35715 Add the option to specify a target plane to package services
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- esp/services/ws_workunits/ws_workunitsService.cpp
+++ esp/services/ws_workunits/ws_workunitsService.cpp
@@ -5614,6 +5614,7 @@ void CWsWorkunitsEx::publishEclDefinition(IEspContext &context, const char *targ
     publishReq->setDfuOverwrite(req.getDfuOverwrite());
     publishReq->setDfuQueue(req.getDfuQueue());
     publishReq->setDfuPublisherWuid(req.getDfuPublisherWuid());
+    publishReq->setDfuTargetPlane(req.getDfuTargetPlane());
 
     publishReq->setAllowForeignFiles(req.getAllowForeign());
     publishReq->setUpdateDfs(req.getUpdateDfs());
